91de7641fac9b21a73492b88d2cbdb705ba070a2
Bhushan Ghorpade <bghorpade1@gmail.com>
2019-09-24 21:47:46 +0530

    Committing Data Types and variables

---
+++ BasicCoreJava/src/variabeAndDataTypes/FirstJavaClass.java
@@ -0,0 +1,29 @@
+package variabeAndDataTypes;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) 
+	{
+		// Widely used variables
+		int a=10; // for integer values
+		char b='A'; // for characters
+		boolean c=true; // for boolean values
+		double d = 6.895; //for decimal values
+		String Name ="Bhushan"; // for string values ** String is not a data type
+		
+		// other data type:
+		// for Numbers
+		byte x =10;
+		short y=45;
+		long z=789456789;
+		
+		//for decimal values
+		float f = 67.8f;
+		
+		System.out.println(a);
+		
+		
+
+	}
+
+}
